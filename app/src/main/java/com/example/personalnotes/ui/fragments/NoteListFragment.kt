c590461070eca5eae707e70979399a77d60adeff
Alex Schwartzman <aleks.schwartzman@gmail.com>
2020-05-20 14:40:20 +0200

    Added action bar to the list fragment. Looks nicer now. All remaining fragments are still to-do

--- app/src/main/java/com/example/personalnotes/ui/fragments/NoteListFragment.kt
+++ app/src/main/java/com/example/personalnotes/ui/fragments/NoteListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
@@ -34,6 +35,8 @@ class NoteListFragment : Fragment() {
         binding.newItemClickListener = View.OnClickListener {
             findNavController().navigate(NoteListFragmentDirections.actionViewPagerFragmentToNoteCreateFragment())
         }
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+
         return binding.root
     }
 }
